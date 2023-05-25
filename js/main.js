b7c20fc9e2d3db557f07b896bcbce2785d2cc8c9
kim bo sung <njunju77@nate.com>
2023-05-25 17:44:30 +0900

    23/05/25

--- js/main.js
+++ js/main.js
@@ -1,34 +1,30 @@
-const { url } = require("css-tree");
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "vertical",
+  loop: true,
 
-document.addEventListener("DOMContentLoaded", () => {
-  const swiper = new Swiper(".swiper", {
-    // Optional parameters
-    direction: "vertical",
-    loop: true,
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
 
-    // If we need pagination
-    pagination: {
-      el: ".swiper-pagination",
-    },
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 
-    // Navigation arrows
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
+//컬렉션
+const backImg = document.querySelector(".collection_inner");
+const hoverBox = document.querySelectorAll(".collection_inner div");
 
-    // And if we need scrollbar
-    scrollbar: {
-      el: ".swiper-scrollbar",
-    },
-  });
-  //컬렉션
-  const backImg = document.querySelector(".collection_inner");
-  const hoverBox = document.querySelectorAll(".collection_inner div");
-
-  /*  for (let i = 0; i < hoverBox.length; i++) {
+/*  for (let i = 0; i < hoverBox.length; i++) {
     hoverBox[i].addEventListener("mouseover", function () {
 
     });
   } */
-});
