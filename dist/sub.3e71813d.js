d5317f82b95326ba593f93a4ef15052785671aeb
kim bo sueong <njunju77@nate.com>
2023-06-05 04:05:02 +0900

    23/06/05 4:04

---
+++ dist/sub.3e71813d.js
@@ -0,0 +1,412 @@
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+parcelRequire = (function (modules, cache, entry, globalName) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof parcelRequire === 'function' && parcelRequire;
+  var nodeRequire = typeof require === 'function' && require;
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof parcelRequire === 'function' && parcelRequire;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        // Try the node require function if it exists.
+        if (nodeRequire && typeof name === 'string') {
+          return nodeRequire(name);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+      localRequire.cache = {};
+
+      var module = cache[name] = new newRequire.Module(name);
+
+      modules[name][0].call(module.exports, localRequire, module, module.exports, this);
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+  newRequire.register = function (id, exports) {
+    modules[id] = [function (require, module) {
+      module.exports = exports;
+    }, {}];
+  };
+
+  var error;
+  for (var i = 0; i < entry.length; i++) {
+    try {
+      newRequire(entry[i]);
+    } catch (e) {
+      // Save first error but execute all entries
+      if (!error) {
+        error = e;
+      }
+    }
+  }
+
+  if (entry.length) {
+    // Expose entry point to Node, AMD or browser globals
+    // Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+    var mainExports = newRequire(entry[entry.length - 1]);
+
+    // CommonJS
+    if (typeof exports === "object" && typeof module !== "undefined") {
+      module.exports = mainExports;
+
+    // RequireJS
+    } else if (typeof define === "function" && define.amd) {
+     define(function () {
+       return mainExports;
+     });
+
+    // <script>
+    } else if (globalName) {
+      this[globalName] = mainExports;
+    }
+  }
+
+  // Override the current require with this new one
+  parcelRequire = newRequire;
+
+  if (error) {
+    // throw error from earlier, _after updating parcelRequire_
+    throw error;
+  }
+
+  return newRequire;
+})({"js/sub.js":[function(require,module,exports) {
+//menuMouseEvent
+var visual = document.querySelector(".visual");
+var headerOuter = document.querySelector(".header_outer");
+var header = document.querySelector(".header");
+var logo = document.querySelector(".header .logo");
+var menu = document.querySelectorAll("li.menu");
+var gnb = document.querySelector(".gnb");
+
+//
+//
+for (var i = 0; i < menu.length; i++) {
+  menu[i].addEventListener("mouseout", function () {
+    headerOuter.style.height = "auto";
+    //headerOuter.style.borderTop = "1px solid #000";
+    //headerOuter.style.borderBottom = "1px solid #000";
+    headerOuter.style.backgroundColor = "";
+    logo.style.color = "#000";
+    header.style.color = "#000";
+    headerOuter.style.transition = "0.3s";
+    //gnb.style = "filter:brightness(1);";
+  });
+
+  menu[i].addEventListener("mouseover", function () {
+    headerOuter.style.height = "180px";
+    headerOuter.style.border = "none";
+    headerOuter.style.backgroundColor = "#fcf9ee";
+    logo.style.color = "#000";
+    header.style.color = "#000";
+    headerOuter.style.transition = "0.3s";
+    //gnb.style = "filter:brightness(0);";
+  });
+}
+
+//search
+/* const searchBtn = document.querySelector(".gnb .search_btn");
+const search = document.querySelector(".search");
+const searchClose = document.querySelector(".search span");
+
+searchBtn.addEventListener("click", function () {
+  search.style.display = "block";
+});
+searchClose.addEventListener("click", function () {
+  search.style.display = "none";
+}); */
+//
+//subList
+var item = [{
+  src: "/images/sub_cologne/sub_item01.png",
+  en: "Blackberry & Bay Cologne",
+  ko: "블랙베리 앤 베이 코롱",
+  price: "₩ 218,000"
+}, {
+  src: "./images/sub_cologne/sub_item02.png",
+  en: "Lime Basil & Mandarin Cologne",
+  ko: "라임 바질 앤 만다린 코롱",
+  price: "₩ 218,000"
+}, {
+  src: "./images/sub_cologne/sub_item03.png",
+  en: "Wood Sage & Sea Salt Cologne",
+  ko: "우드세이지 앤 씨쏠트 코롱",
+  price: "₩ 218,000"
+}, {
+  src: "/images/sub_cologne/sub_item04.png",
+  en: "Star Magnolia Cologne",
+  ko: "스타 매그놀리아 코롱",
+  price: "₩ 225,000"
+}, {
+  src: "/images/sub_cologne/sub_item05.png",
+  en: "Nashi Blossom Cologne",
+  ko: "나시 블로썸 코롱",
+  price: "₩ 225,000"
+}, {
+  src: "/images/sub_cologne/sub_item06.png",
+  en: "Waterlily Cologne",
+  ko: "워터릴리 코롱",
+  price: "₩ 225,000"
+}, {
+  src: "/images/sub_cologne/sub_item07.png",
+  en: "Special-Edition Red Roses Cologne",
+  ko: "스페셜 에디션 레드 로즈 코롱",
+  price: "₩ 160,000"
+}, {
+  src: "/images/sub_cologne/sub_item08.png",
+  en: "Rose Water & Vanilla Cologne",
+  ko: "로즈 워터 앤 바닐라 코롱",
+  price: "₩ 116,000"
+}, {
+  src: "/images/sub_cologne/sub_item09.png",
+  en: "Rose Blush Cologne",
+  ko: "로즈 블러쉬 코롱",
+  price: "₩ 116,000"
+}, {
+  src: "/images/sub_cologne/sub_item10.png",
+  en: "The Golden & Sweet Duo",
+  ko: "더 골든 앤 스윗 듀오",
+  price: "₩ 220,000"
+}, {
+  src: "/images/sub_cologne/sub_item11.png",
+  en: "Spirited & Addictive Duo",
+  ko: "스피릿 앤 에딕티드 듀오",
+  price: "₩ 220,000"
+}, {
+  src: "/images/sub_cologne/sub_item12.png",
+  en: "Fruity & Sweet Duo",
+  ko: "프루티 앤 스윗 듀오",
+  price: "₩ 220,000"
+}];
+var subImg = document.querySelectorAll(".item_list .item .img_box img");
+var subTextEn = document.querySelectorAll(".item_list .item .text .en_title");
+var subTextKo = document.querySelectorAll(".item_list .item .text .ko_title");
+var price = document.querySelectorAll(".item_list .item .text .price");
+console.log(subImg);
+console.log(subTextEn);
+console.log(subTextKo);
+console.log(price);
+for (var _i = 0; _i < item.length; _i++) {
+  subImg[_i].src = item[_i].src;
+  subTextEn[_i].textContent = item[_i].en;
+  subTextKo[_i].textContent = item[_i].ko;
+  price[_i].textContent = item[_i].price;
+}
+},{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+var global = arguments[3];
+var OVERLAY_ID = '__parcel__error__overlay__';
+var OldModule = module.bundle.Module;
+function Module(moduleName) {
+  OldModule.call(this, moduleName);
+  this.hot = {
+    data: module.bundle.hotData,
+    _acceptCallbacks: [],
+    _disposeCallbacks: [],
+    accept: function (fn) {
+      this._acceptCallbacks.push(fn || function () {});
+    },
+    dispose: function (fn) {
+      this._disposeCallbacks.push(fn);
+    }
+  };
+  module.bundle.hotData = null;
+}
+module.bundle.Module = Module;
+var checkedAssets, assetsToAccept;
+var parent = module.bundle.parent;
+if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
+  var hostname = "" || location.hostname;
+  var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59936" + '/');
+  ws.onmessage = function (event) {
+    checkedAssets = {};
+    assetsToAccept = [];
+    var data = JSON.parse(event.data);
+    if (data.type === 'update') {
+      var handled = false;
+      data.assets.forEach(function (asset) {
+        if (!asset.isNew) {
+          var didAccept = hmrAcceptCheck(global.parcelRequire, asset.id);
+          if (didAccept) {
+            handled = true;
+          }
+        }
+      });
+
+      // Enable HMR for CSS by default.
+      handled = handled || data.assets.every(function (asset) {
+        return asset.type === 'css' && asset.generated.js;
+      });
+      if (handled) {
+        console.clear();
+        data.assets.forEach(function (asset) {
+          hmrApply(global.parcelRequire, asset);
+        });
+        assetsToAccept.forEach(function (v) {
+          hmrAcceptRun(v[0], v[1]);
+        });
+      } else if (location.reload) {
+        // `location` global exists in a web worker context but lacks `.reload()` function.
+        location.reload();
+      }
+    }
+    if (data.type === 'reload') {
+      ws.close();
+      ws.onclose = function () {
+        location.reload();
+      };
+    }
+    if (data.type === 'error-resolved') {
+      console.log('[parcel] ✨ Error resolved');
+      removeErrorOverlay();
+    }
+    if (data.type === 'error') {
+      console.error('[parcel] 🚨  ' + data.error.message + '\n' + data.error.stack);
+      removeErrorOverlay();
+      var overlay = createErrorOverlay(data);
+      document.body.appendChild(overlay);
+    }
+  };
+}
+function removeErrorOverlay() {
+  var overlay = document.getElementById(OVERLAY_ID);
+  if (overlay) {
+    overlay.remove();
+  }
+}
+function createErrorOverlay(data) {
+  var overlay = document.createElement('div');
+  overlay.id = OVERLAY_ID;
+
+  // html encode message and stack trace
+  var message = document.createElement('div');
+  var stackTrace = document.createElement('pre');
+  message.innerText = data.error.message;
+  stackTrace.innerText = data.error.stack;
+  overlay.innerHTML = '<div style="background: black; font-size: 16px; color: white; position: fixed; height: 100%; width: 100%; top: 0px; left: 0px; padding: 30px; opacity: 0.85; font-family: Menlo, Consolas, monospace; z-index: 9999;">' + '<span style="background: red; padding: 2px 4px; border-radius: 2px;">ERROR</span>' + '<span style="top: 2px; margin-left: 5px; position: relative;">🚨</span>' + '<div style="font-size: 18px; font-weight: bold; margin-top: 20px;">' + message.innerHTML + '</div>' + '<pre>' + stackTrace.innerHTML + '</pre>' + '</div>';
+  return overlay;
+}
+function getParents(bundle, id) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return [];
+  }
+  var parents = [];
+  var k, d, dep;
+  for (k in modules) {
+    for (d in modules[k][1]) {
+      dep = modules[k][1][d];
+      if (dep === id || Array.isArray(dep) && dep[dep.length - 1] === id) {
+        parents.push(k);
+      }
+    }
+  }
+  if (bundle.parent) {
+    parents = parents.concat(getParents(bundle.parent, id));
+  }
+  return parents;
+}
+function hmrApply(bundle, asset) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return;
+  }
+  if (modules[asset.id] || !bundle.parent) {
+    var fn = new Function('require', 'module', 'exports', asset.generated.js);
+    asset.isNew = !modules[asset.id];
+    modules[asset.id] = [fn, asset.deps];
+  } else if (bundle.parent) {
+    hmrApply(bundle.parent, asset);
+  }
+}
+function hmrAcceptCheck(bundle, id) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return;
+  }
+  if (!modules[id] && bundle.parent) {
+    return hmrAcceptCheck(bundle.parent, id);
+  }
+  if (checkedAssets[id]) {
+    return;
+  }
+  checkedAssets[id] = true;
+  var cached = bundle.cache[id];
+  assetsToAccept.push([bundle, id]);
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    return true;
+  }
+  return getParents(global.parcelRequire, id).some(function (id) {
+    return hmrAcceptCheck(global.parcelRequire, id);
+  });
+}
+function hmrAcceptRun(bundle, id) {
+  var cached = bundle.cache[id];
+  bundle.hotData = {};
+  if (cached) {
+    cached.hot.data = bundle.hotData;
+  }
+  if (cached && cached.hot && cached.hot._disposeCallbacks.length) {
+    cached.hot._disposeCallbacks.forEach(function (cb) {
+      cb(bundle.hotData);
+    });
+  }
+  delete bundle.cache[id];
+  bundle(id);
+  cached = bundle.cache[id];
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    cached.hot._acceptCallbacks.forEach(function (cb) {
+      cb();
+    });
+    return true;
+  }
+}
+},{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js","js/sub.js"], null)
+//# sourceMappingURL=/sub.3e71813d.js.map
---
+++ js/sub.js
@@ -0,0 +1,77 @@
+//menuMouseEvent
+const visual = document.querySelector(".visual");
+const headerOuter = document.querySelector(".header_outer");
+const header = document.querySelector(".header");
+const logo = document.querySelector(".header .logo");
+const menu = document.querySelectorAll("li.menu");
+const gnb = document.querySelector(".gnb");
+
+//
+//
+for (let i = 0; i < menu.length; i++) {
+  menu[i].addEventListener("mouseout", function () {
+    headerOuter.style.height = "auto";
+    //headerOuter.style.borderTop = "1px solid #000";
+    //headerOuter.style.borderBottom = "1px solid #000";
+    headerOuter.style.backgroundColor = "";
+    logo.style.color = "#000";
+    header.style.color = "#000";
+    headerOuter.style.transition = "0.3s";
+    //gnb.style = "filter:brightness(1);";
+  });
+
+  menu[i].addEventListener("mouseover", function () {
+    headerOuter.style.height = "180px";
+    headerOuter.style.border = "none";
+    headerOuter.style.backgroundColor = "#fcf9ee";
+    logo.style.color = "#000";
+    header.style.color = "#000";
+    headerOuter.style.transition = "0.3s";
+    //gnb.style = "filter:brightness(0);";
+  });
+}
+
+//search
+/* const searchBtn = document.querySelector(".gnb .search_btn");
+const search = document.querySelector(".search");
+const searchClose = document.querySelector(".search span");
+
+searchBtn.addEventListener("click", function () {
+  search.style.display = "block";
+});
+searchClose.addEventListener("click", function () {
+  search.style.display = "none";
+}); */
+//
+//subList
+const item = [
+  { src: "/images/sub_cologne/sub_item01.png", en: "Blackberry & Bay Cologne", ko: "블랙베리 앤 베이 코롱", price: "₩ 218,000" },
+  { src: "./images/sub_cologne/sub_item02.png", en: "Lime Basil & Mandarin Cologne", ko: "라임 바질 앤 만다린 코롱", price: "₩ 218,000" },
+  { src: "./images/sub_cologne/sub_item03.png", en: "Wood Sage & Sea Salt Cologne", ko: "우드세이지 앤 씨쏠트 코롱", price: "₩ 218,000" },
+
+  { src: "/images/sub_cologne/sub_item04.png", en: "Star Magnolia Cologne", ko: "스타 매그놀리아 코롱", price: "₩ 225,000" },
+  { src: "/images/sub_cologne/sub_item05.png", en: "Nashi Blossom Cologne", ko: "나시 블로썸 코롱", price: "₩ 225,000" },
+  { src: "/images/sub_cologne/sub_item06.png", en: "Waterlily Cologne", ko: "워터릴리 코롱", price: "₩ 225,000" },
+  { src: "/images/sub_cologne/sub_item07.png", en: "Special-Edition Red Roses Cologne", ko: "스페셜 에디션 레드 로즈 코롱", price: "₩ 160,000" },
+  { src: "/images/sub_cologne/sub_item08.png", en: "Rose Water & Vanilla Cologne", ko: "로즈 워터 앤 바닐라 코롱", price: "₩ 116,000" },
+  { src: "/images/sub_cologne/sub_item09.png", en: "Rose Blush Cologne", ko: "로즈 블러쉬 코롱", price: "₩ 116,000" },
+  { src: "/images/sub_cologne/sub_item10.png", en: "The Golden & Sweet Duo", ko: "더 골든 앤 스윗 듀오", price: "₩ 220,000" },
+  { src: "/images/sub_cologne/sub_item11.png", en: "Spirited & Addictive Duo", ko: "스피릿 앤 에딕티드 듀오", price: "₩ 220,000" },
+  { src: "/images/sub_cologne/sub_item12.png", en: "Fruity & Sweet Duo", ko: "프루티 앤 스윗 듀오", price: "₩ 220,000" },
+];
+
+const subImg = document.querySelectorAll(".item_list .item .img_box img");
+const subTextEn = document.querySelectorAll(".item_list .item .text .en_title");
+const subTextKo = document.querySelectorAll(".item_list .item .text .ko_title");
+const price = document.querySelectorAll(".item_list .item .text .price");
+console.log(subImg);
+console.log(subTextEn);
+console.log(subTextKo);
+console.log(price);
+
+for (let i = 0; i < item.length; i++) {
+  subImg[i].src = item[i].src;
+  subTextEn[i].textContent = item[i].en;
+  subTextKo[i].textContent = item[i].ko;
+  price[i].textContent = item[i].price;
+}
