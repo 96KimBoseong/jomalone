d563439882c3a31c41fa11a763d8104a5cf0dd63
kim bo sung <njunju77@nate.com>
2023-06-08 17:56:18 +0900

    23/06/08

--- dist/main.fb6bbcaf.js
+++ dist/main.fb6bbcaf.js
@@ -19637,7 +19637,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54122" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56046" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
