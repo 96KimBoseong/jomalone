c008ce89389edc20998933890caac8d45992361d
kimbosung <njunju77@nate.com>
2023-06-30 14:58:58 +0900

    23/06/30

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+const { switchCase } = require("babel-types");
 const { result } = require("lodash");
 
 const swiperThums = new Swiper(".swiper_thums", {
@@ -256,12 +257,56 @@ const bestDelete = document.querySelector('.best_modal .delete')
 const bestImg = document.querySelector('.modal_img img')
 const bestTxt = document.querySelectorAll('.modal_txt')
 
+const modalEn = document.querySelector('.best_tit_en')
+const modalKo = document.querySelector('.best_tit_ko')
+const modalInfo = document.querySelector('.best_info')
+const modalPrice = document.querySelector('.best_price')
 
 
-for(let i = 1; i <bestPreview.length; i++){
+
+for(let i = 0; i <bestPreview.length; i++){
   bestPreview[i].addEventListener("click",()=>{
     bestModal.style.display = "block"
-    bestImg.src = `./images/main/best_0${i}.png`
+    bestImg.src = `./images/main/best_0${i+2}.png`
+    switch(i){
+      case 0 : 
+      modalEn.textContent = 'Mini Luxuries'
+      modalKo.textContent = '미니 럭셔리즈'
+      modalInfo.textContent='조 말론 런던의 세 가지 시그니처 센트를 여행용 사이즈로 만나보세요. 활기 넘치는 우드 세이지 앤 씨 솔트 코롱과, 풍성한 질감의 피오니 앤 블러쉬 스웨이드 바디 크림으로 피부를 가꾸고 감미로운 잉글리쉬 페어 앤 프리지아 미니어처 캔들로 분위기를 꾸며보세요. 트래블 파우치에 구성됩니다.'
+      modalPrice.textContent = '₩ 69,000'
+      break;
+      case 1: 
+      modalEn.textContent = 'Blackberry & Bay Cologne'
+      modalKo.textContent = '블랙베리 앤 베이 코롱'
+      modalInfo.textContent='순수의 향, 블랙베리를 따던 어린 시절의 추억, 블랙베리로 물든 입술과 손. 이제 막 수확한 월계수 잎의 신선함에 톡 쏘는 블랙베리 과즙을 가미하였습니다.매력적이고 생기 넘치는 상쾌한 느낌의 향입니다.'
+      modalPrice.textContent = '₩ 218,000'
+      break;
+      case 2:
+      modalEn.textContent = 'Vanilla & Anise Cologne'
+      modalKo.textContent = '바닐라 앤 아니스 코롱'
+      modalInfo.textContent='바닐라 향을 현대적으로 해석한 향. 섬세한 바닐라 오키드와 진한 카라멜 향이 만나 따뜻하게 감싸주는 느낌을 선사합니다.'
+      modalPrice.textContent = '₩ 218,000'
+      break;
+      case 3:
+        modalEn.textContent = 'Wild Bluebell Body & Hand Wash'
+      modalKo.textContent = '와일드 블루벨 바디 앤 핸드워시'
+      modalInfo.textContent='마음에 여유를 더하는 와일드 블루벨 바디 앤 핸드 워시로 당신의 데일리 루틴을 완성해보세요. 자연 유래 글리세린과 메도우폼 씨드 오일이 함유된 포뮬러가 벨벳처럼 부드러운 거품을 내며, 피부를 클렌징해주고 부드럽게 가꿀 수 있도록 도와줍니다. 마음을 사로잡는 라이트 플로랄 향을 피부에 은은하게 남겨보세요.'
+      modalPrice.textContent = '₩ 75,000'
+      break;
+      case 4:
+        modalEn.textContent = 'White Jasmine & Mint Cologne'
+        modalKo.textContent = '화이트 자스민 앤 민트 코롱'
+        modalInfo.textContent='햇살이 퍼지는 영국 시골 정원의 아침. 바로 딴 야생 민트와 자스민, 백합, 오렌지 꽃과 장미로 만들어진 부케의 향이 어우러진 마법같은 향을 선사합니다.'
+        modalPrice.textContent = '₩ 218,000'
+        break;
+        case 5:
+          modalEn.textContent = 'Peony & Blush Suede Cologne'
+          modalKo.textContent = '피오니 앤 블러쉬 스웨이드 코롱'
+          modalInfo.textContent='꽃의 귀족, 작약의 화려함을 담은 향. 화려하게 핀 작약에 더해진 붉은 사과의 향기로운 과즙과 순수한 자스민, 장미 그리고 카네이션, 블러쉬 스웨이드의 부드러운 관능미가 여운을 남깁니다.'
+          modalPrice.textContent = '₩ 218,000' 
+
+    }
+    
   })
 }
 
